73b58af52b87a6ee67b1db641fb5d3a508a18aa2
salmanpatnee <salmanpatni92@gmail.com>
2025-01-01 22:07:35 +0500

    Build an api to create expense

---
+++ app/api/expenses/route.ts
@@ -0,0 +1,32 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const createExpenseSchema = z.object({
+    title: z.string().min(3).max(1000),
+    amount: z.number().min(0)
+})
+
+export const POST = async (request: NextRequest) => {
+
+    const body = await request.json();
+
+    const validation = createExpenseSchema.safeParse(body);
+
+    if (!validation.success) {
+        return NextResponse.json(validation.error.format(), { status: 400 });
+    }
+
+    try {
+        const newExpense = await prisma.expense.create({
+            data: {
+                title: body.title,
+                amount: body.amount,
+            },
+        });
+
+        return NextResponse.json(newExpense, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: "Failed to create expense" }, { status: 500 });
+    }
+};
