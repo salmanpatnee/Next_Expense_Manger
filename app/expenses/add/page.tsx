8c46d29d2cc4b19946a5cfb66b6521d78205aa62
salmanpatnee <salmanpatni92@gmail.com>
2025-01-02 00:13:36 +0500

    Handle error handling

--- app/expenses/add/page.tsx
+++ app/expenses/add/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
+import { MdErrorOutline } from "react-icons/md";
 interface ExpenseForm {
   title: string;
   amount: number;
@@ -12,28 +13,43 @@ interface ExpenseForm {
 const AddExpensePage = () => {
   const { register, handleSubmit } = useForm<ExpenseForm>();
   const router = useRouter();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post(`/api/expenses`, data);
-        router.push(`/expenses`)
-      })}
-    >
-      <TextField.Root
-        placeholder="Internet Fee"
-        size={"3"}
-        {...register("title")}
-      />
-      <TextField.Root
-        type="number"
-        placeholder="1200"
-        size={"3"}
-        {...register("amount")}
-      />
-      <Button>Add Expense</Button>
-    </form>
+    <div className="max-w-xl ">
+      {error && (
+        <Callout.Root mb={"3"}>
+          <Callout.Icon>
+            <MdErrorOutline />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="space-y-5"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post(`/api/expenses`, data);
+            router.push(`/expenses`);
+            setError('')
+          } catch (error) {
+            setError("An unexpected error is occured.");
+          }
+        })}
+      >
+        <TextField.Root
+          placeholder="Internet Fee"
+          size={"3"}
+          {...register("title")}
+        />
+        <TextField.Root
+          type="number"
+          placeholder="1200"
+          size={"3"}
+          {...register("amount")}
+        />
+        <Button>Add Expense</Button>
+      </form>
+    </div>
   );
 };
 
