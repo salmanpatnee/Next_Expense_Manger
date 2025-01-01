fb386f68b2ab98568b54b53d63f2f4ac2affb21c
salmanpatnee <salmanpatni92@gmail.com>
2025-01-01 17:11:02 +0500

    Style the active link in the Navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,25 +1,40 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React from "react";
 import { TfiWrite } from "react-icons/tfi";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
-    { label: 'Dashboard', href: '/' },
-    { label: 'Expenses', href: '/expenses' },
-  ]
+    { label: "Dashboard", href: "/" },
+    { label: "Expenses", href: "/expenses" },
+  ];
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-      <Link href="/"><TfiWrite /></Link>
-      <ul className='flex space-x-6'>
-        {links.map(link => 
-          <Link 
-            key={link.href} 
-            className='text-zinc-500 hover:text-zinc-800 transition-colors' 
-            href={link.href}>{link.label}</Link>)}
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <TfiWrite />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classnames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
